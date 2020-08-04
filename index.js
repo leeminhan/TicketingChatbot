a796deeef890c53683c3ad003e9765027fb152bd
leeminhan <minhan.lmh@gmail.com>
2020-08-04 18:05:44 +0800

    DialogFlow Auth

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const dialogFlowRoute = require("./routes/dialogFlowRoute")
 
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send({"hello": "there"})
 })
 
+require('./routes/dialogFlowRoute')(app);
+// app.use('/api/df_text_query', dialogFlowRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
-    console.log("App running on port 5000")
+    console.log("App running on port 8000")
 });
+
+// module.exports = app;
--- routes/dialogFlowRoute.js
+++ routes/dialogFlowRoute.js
@@ -1,10 +1,35 @@
+const express = require('express');
+const router = new express.Router();
+const dialogflow = require('dialogflow');
+const config = require("../config/keys")
+
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(config.googleProjectId, config.dialogFlowSessionId);
+
 module.exports = app => {
-    
-    // requests with text from bot
-    app.post('/api/df_text_query', (req,res) => {
-        res.send({
-            "do": "text query"
-        })
+
+    // requests with text from bot (frontend)
+    app.post('/api/df_text_query', async(req,res) => {
+        
+        console.log(req.body.text)
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+              text: {
+                // The query to send to the dialogflow agent
+                text: req.body.text,
+                // The language used by the client (en-US)
+                languageCode: config.dialogFlowSessionLanguageCode,
+              },
+            },
+          };
+
+
+        let responses = await sessionClient.detectIntent(request)
+        console.log(responses)
+        
+        res.send(responses[0].queryResult)
     })
 
     // requests with event from bot
@@ -15,3 +40,36 @@ module.exports = app => {
     })
 
 }
+
+// requests with text from bot (frontend)
+// router.post('/api/df_text_query', async(req,res) => {
+    
+//     console.log(req.body.text)
+
+//     const request = {
+//         session: sessionPath,
+//         queryInput: {
+//             text: {
+//             // The query to send to the dialogflow agent
+//             text: req.body.text,
+//             // The language used by the client (en-US)
+//             languageCode: config.dialogFlowSessionLanguageCode,
+//             },
+//         },
+//         };
+
+
+//     let responses = await sessionClient.detectIntent(request)
+//     console.log(responses)
+    
+//     res.send(responses[0].queryResult)
+// })
+
+// // requests with event from bot
+// router.post('/api/df_event_query', (req,res) => {
+//     res.send({
+//         "do": "event query"
+//     })
+// })
+
+// module.exports = router;
