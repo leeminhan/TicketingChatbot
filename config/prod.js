66a7ba32e3c1be85e84c07b72a73f188aa1b1dc0
leeminhan <minhan.lmh@gmail.com>
2020-08-05 00:41:22 +0800

    Fixing issues with env variables

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     dialogFlowSessionId: process.env.DIALOG_FLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googleprivateKey: process.env.GOOGLE_PRIVATE_KEY
+    googleprivateKey: JSON.parse(process.env.GOOGLE_GOOGLE_PRIVATE_KEY)
+    // googleprivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
