e0d57546104dde3e01b400205ba85debadfc29df
leeminhan <minhan.lmh@gmail.com>
2020-08-05 01:01:37 +0800

    Reverting without JSON.parse

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     dialogFlowSessionId: process.env.DIALOG_FLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: JSON.parse(process.env.GOOGLE_GOOGLE_PRIVATE_KEY)
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
