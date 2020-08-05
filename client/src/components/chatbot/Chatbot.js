8444aab5a5491f317b032c7b8fb81b658cabf19f
leeminhan <minhan.lmh@gmail.com>
2020-08-06 01:57:00 +0800

    renderMessages for user and bot  and handleInput from user

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
+import Message from './Message';
+
 class Chatbot extends Component {
+
+    messagesEnd;
 
     constructor(props) {
         super(props);
+
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this.state = {
             messages: []
         }
@@ -50,6 +56,33 @@ class Chatbot extends Component {
         }
     }
 
+    // when component has been rendered to the DOM
+    componentDidMount() {
+        this.df_event_query('welcome') //welcome eventQuery
+    }
+
+    componentDidUpdate() {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
+    
+
+    renderMessages(stateMessages) {
+        if(stateMessages) {
+            return stateMessages.map((message, i) => {
+                return <Message key={i} speaks= {message.speaks} text={message.msg.text.text} />
+            })
+        } else {
+            return null;
+        }
+    }
+
+    _handleInputKeyPress(e){
+        if(e.key == 'Enter'){
+            this.df_text_query(e.target.value)
+            e.target.value = ''
+        }
+    }
+
     // Outer Div for the Chatbot frame
     // Inner Div for where all the messages will be displayed
     // overflow is for scrolling
@@ -58,7 +91,11 @@ class Chatbot extends Component {
             <div style={{height:400, width:400, float: 'right'}}>
                 <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}> 
                     <h2>Chatbot</h2>
-                    <input type="text"/>
+                    {this.renderMessages(this.state.messages)}
+                    <div ref={(el) => {this.messagesEnd =el}} 
+                        style={{float:'left', clear: 'both'}}>
+                    </div>
+                    <input type="text" onKeyPress={this._handleInputKeyPress}/>
                 </div>
 
             </div>
