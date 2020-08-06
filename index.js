821cea483a29e8ea97ae13ac16fda4e179c242d6
leeminhan <minhan.lmh@gmail.com>
2020-08-07 02:10:02 +0800

    Productionizing code

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get('/', (req, res) => {
 require('./routes/dialogFlowRoute')(app);
 // app.use('/api/df_text_query', dialogFlowRoute);
 
+if (process.env.NODE_ENV === 'production') {
+    // js and css files
+    app.use(express.static('client/build'));
+
+    // index.html for all page routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
