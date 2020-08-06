12f158cdac3be9ad0fcf43b49aea1d43dd613271
leeminhan <minhan.lmh@gmail.com>
2020-08-06 19:52:06 +0800

    Added rendering of Card component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -4,6 +4,7 @@ import Cookies from 'universal-cookie';
 import {v4 as uuid} from 'uuid';
 
 import Message from './Message';
+import Card from './Card';
 
 const cookies = new Cookies();
 
@@ -23,7 +24,6 @@ class Chatbot extends Component {
         if (cookies.get('userID') === undefined) {
             cookies.set('userID', uuid(), { path: '/' });
         }
-        console.log((cookies.get('userID')))
     }
 
     async df_text_query(queryText) {
@@ -43,7 +43,7 @@ class Chatbot extends Component {
         
         // messages from bot
         for (let msg of res.data.fulfillmentMessages) {
-            
+            // console.log(JSON.stringify(msg))
             let says = {
                 speaks: "bot",
                 msg: msg
@@ -74,12 +74,41 @@ class Chatbot extends Component {
     componentDidUpdate() {
         this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     }
+
+    renderCards(cards){
+        return cards.map((card, i) => 
+            <Card key={i} payload={card.structValue}/>
+        )
+    }
+
+    renderOneMessage(message, i){
+        
+        if(message.msg && message.msg.text && message.msg.text.text){
+            return <Message key={i} speaks= {message.speaks} text={message.msg.text.text} />
+
+        } else if (message.msg && message.msg.payload.fields.cards){
+            return <div key={i}>
+                <div className='card-panel grey lighten-5 z-depth-1'>
+                    <div className="col s2">
+                        <a href="/" className="btn-floating btn-large waves-effect waves-light red">{message.speaks}</a>
+                    </div>
+
+                    <div style={{overflow: 'auto', overFlowY: 'scroll'}}>
+                        <div style={{ height: 300, width:message.msg.payload.fields.cards.listValue.values.length * 270}}>
+                            {this.renderCards(message.msg.payload.fields.cards.listValue.values)}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        }
+    }
     
 
     renderMessages(stateMessages) {
         if(stateMessages) {
             return stateMessages.map((message, i) => {
-                return <Message key={i} speaks= {message.speaks} text={message.msg.text.text} />
+                return this.renderOneMessage(message, i)
             })
         } else {
             return null;
@@ -98,15 +127,27 @@ class Chatbot extends Component {
     // overflow is for scrolling
     render() {
         return (
-            <div style={{height:400, width:400, float: 'right'}}>
-                <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}> 
-                    <h2>Chatbot</h2>
+            <div style={{height:500, width:500, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgrey'}}>
+            
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="/" className="brand-logo">ChatBot</a>
+                    </div>
+                </nav>
+            
+            
+                <div id="chatbot"  style={{ minHeight: 388, maxHeight: 388, width:'100%', overflow: 'auto'}}>
                     {this.renderMessages(this.state.messages)}
                     <div ref={(el) => {this.messagesEnd =el}} 
                         style={{float:'left', clear: 'both'}}>
                     </div>
-                    <input type="text" onKeyPress={this._handleInputKeyPress}/>
                 </div>
+                
+                <div className=" col s12" >
+                    <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} ref={(input) => { this.talkInput = input; }} placeholder="type a message:"  onKeyPress={this._handleInputKeyPress} id="user_says" type="text" />
+                </div>
+                
+                
 
             </div>
         )
